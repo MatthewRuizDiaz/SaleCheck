f9b859d6c6fd61404806e0c754f9660d6a43d233
Matthew Ruiz Diaz <matthew.ruizdiaz@gmail.com>
2026-01-20 01:57:35 -0700

    small fix for logs

--- backend/index.js
+++ backend/index.js
@@ -200,5 +200,5 @@ app.get('/products/asin/:asin', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('listening on port ${PORT}')
+    console.log(`listening on port ${PORT}`)
 })
