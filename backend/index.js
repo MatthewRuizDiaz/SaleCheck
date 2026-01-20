4a9366e9fef28162cd98f46ae37ab903a56bc464
Matthew Ruiz Diaz <matthew.ruizdiaz@gmail.com>
2026-01-20 02:02:41 -0700

    Bind to 0.0.0.0 for Railway

--- backend/index.js
+++ backend/index.js
@@ -199,6 +199,6 @@ app.get('/products/asin/:asin', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`listening on port ${PORT}`)
 })
