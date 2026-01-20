873f010d30c82d056bf255c7511965d4f0acb8f8
Matthew Ruiz Diaz <matthew.ruizdiaz@gmail.com>
2026-01-20 01:53:47 -0700

    using dynamic PORT for deployment

--- backend/index.js
+++ backend/index.js
@@ -197,6 +197,8 @@ app.get('/products/asin/:asin', async (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('listening on port ${PORT}')
 })
