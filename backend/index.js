a88e4424661652bcd648ddf83ec5b6e0e05617b7
Matthew Ruiz Diaz <matthew.ruizdiaz@gmail.com>
2026-01-26 18:52:47 -0700

    more railway health check tweaks

--- backend/index.js
+++ backend/index.js
@@ -220,6 +220,7 @@ app.get('/products/asin/:asin', async (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server live on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server live on port ${PORT}`);
 });
